93f8de017d36496afd6209f526bf3ea73b3ba443
belminmujan <mujan.belmin98@gmail.com>
2022-11-02 11:34:16 +0100

    ..

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,6 +30,7 @@ class UserController extends Controller
             "user" => $user
         ], 200);
     }
+    
     public function login(Request $request){
         $validated = $request->validate([
             "email" => "required|email|bail",
@@ -49,8 +50,29 @@ class UserController extends Controller
         ], 200);
     }
 
-    public function authenticateUser(Request $request){
+    public function updateUser(Request $request){
+        Log::Info($request);
         $user = Auth::user();
-        Log::Info($user);
+        if($request->has("first_name")){
+            $v = $request->validate(["first_name" => "required|string|bail"]);
+            $user->first_name = $v["first_name"];
+        }
+        if($request->has("last_name")){
+            $v = $request->validate(["last_name" => "required|string|bail"]);
+            $user->last_name = $v["last_name"];
+        }
+        if($request->has("email")){
+            $v = $request->validate(["email" => "required|string|email|unique:users|bail"]);
+            $user->email = $v["email"];
+        }
+        if($request->has("password")){
+            $v = $request->validate(["password" => "required|confirmed|min:10|bail"]);
+            $user->password = Hash::make($v["password"]);
+        }
+        $user->save();
+        return response()->json([
+            "message" => "Updated succesfully",
+            "user" => $user,
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,12 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::post("login", [UserController::class, 'login']);
-Route::post("register", [UserController::class, 'register']);
+Route::post("login", [UserController::class, "login"]);
+Route::post("register", [UserController::class, "register"]);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware("auth:sanctum")->group(function (){
+    Route::get("/user", function (Request $request) {
+        return $request->user();
+    });
+    Route::post("/update-user", [UserController::class, "updateUser"]);
 });
